8992130246677afafb003d76838cc1c5114b44a6
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-28 18:45:22 -0400

    Q3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -2,14 +2,19 @@
 // ----------
 
 let mostPopularFood = [
-  'bean burritos',
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
-  'buffalo-flavored cauliflower',
+  "bean burritos",
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
+  "buffalo-flavored cauliflower",
 ];
+
+mostPopularFood.sort();
+console.log(mostPopularFood);
+
+//Im not sure how to show the number of the element...do we want the position?
