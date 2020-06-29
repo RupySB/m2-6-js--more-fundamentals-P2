149a7f53844bd9f0ed9523c2a39b6887b032627f
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-28 23:19:00 -0400

    Q7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,15 +2,27 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
 // createRecommendations function
 
+let createRecommendations = (combinations) => {
+  return Object.keys(combinations).map((meals) => {
+    return `With ${meals}, it is best to have ${combinations[meals]}.`;
+  });
+};
+
 // printRecommendations function
+
+let printRecommendations = (combos) => {
+  combos.forEach((combo) => {
+    console.log(`  ${combo}`);
+  });
+};
 
 // function call (done)
 printRecommendations(createRecommendations(foodPairings));
