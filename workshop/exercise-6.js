fc7c1688871b02ebc726889945214eea023ee858
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-28 22:34:02 -0400

    Q6

--- workshop/exercise-6.js
+++ workshop/exercise-6.js
@@ -3,8 +3,8 @@
 
 const staffMembers = [
   {
-    name: 'Jane Smith',
-    title: 'Web Developer II',
+    name: "Jane Smith",
+    title: "Web Developer II",
     skillLevels: {
       javascript: 7,
       python: 4,
@@ -12,8 +12,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Frank Castle',
-    title: 'Web Developer I',
+    name: "Frank Castle",
+    title: "Web Developer I",
     skillLevels: {
       javascript: 4,
       python: 4,
@@ -21,8 +21,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Steve Rogers',
-    title: 'Database Admin',
+    name: "Steve Rogers",
+    title: "Database Admin",
     skillLevels: {
       javascript: 7,
       python: 4,
@@ -30,8 +30,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Chuck Taylor',
-    title: 'Web Developer III',
+    name: "Chuck Taylor",
+    title: "Web Developer III",
     skillLevels: {
       javascript: 8,
       python: 7,
@@ -39,8 +39,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Thor Odinson',
-    title: 'Web Intern',
+    name: "Thor Odinson",
+    title: "Web Intern",
     skillLevels: {
       javascript: 4,
       python: 3,
@@ -50,5 +50,10 @@ const staffMembers = [
 ];
 
 const getData = (arr, key) => {
-  // return something
+  return arr.forEach((object) => {
+    return object[key];
+  });
 };
+console.log(getData(staffMembers, "name", "title"));
+
+//Im not sure why I got undefined as my answer..& I tried using for-of as well.
